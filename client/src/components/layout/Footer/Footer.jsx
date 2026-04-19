ae2d6d85be18592f1f2399931b9a584c82da2a54
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-04-19 17:30:25 +0200

    feat: update Footer component

--- client/src/components/layout/Footer/Footer.jsx
+++ client/src/components/layout/Footer/Footer.jsx
@@ -3,8 +3,19 @@ import styles from './Footer.module.scss';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.inner}>
-        <small>Copyright &copy; New Wave Festival 2026</small>
+      <div className={`container ${styles.inner}`}>
+        <div className={styles.brandBlock}>
+          <h3 className={styles.brand}>NEW WAVE</h3>
+          <small className={styles.copy}>
+            © 2026 New Wave Festival. All rights reserved.
+          </small>
+        </div>
+
+        <div className={styles.links}>
+          <a href='/'>Privacy</a>
+          <a href='/'>Terms</a>
+          <a href='/'>Contact</a>
+        </div>
       </div>
     </footer>
   );
